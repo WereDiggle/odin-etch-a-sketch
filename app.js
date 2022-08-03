26f6f81e84adb9cc1c256fbb5a30f4b66cf2c833
Allison Zou <allison.artemis.zou@gmail.com>
2022-08-02 21:07:55 -0400

    Add button to resize board

--- app.js
+++ app.js
@@ -2,18 +2,26 @@ const $ = (q) => document.querySelector(q);
 const $$ = (q) => document.querySelectorAll(q);
 
 function fillBoard(nRows, nCols) {
+  // Clear old cells
+  $$("#drawboard>div").forEach((cell) => $("#drawboard").removeChild(cell));
+
+  // Add new cells
   $("#drawboard").style.gridTemplateColumns = `repeat(${nCols}, 20px)`;
   $("#drawboard").style.gridTemplateRows = `repeat(${nRows}, 20px)`;
   for (let i = 0; i < nRows * nCols; i++) {
     let cell = document.createElement("div");
+    cell.addEventListener("mouseover", function (e) {
+      this.style.backgroundColor = "cyan";
+    });
     $("#drawboard").appendChild(cell);
   }
 }
 
 fillBoard(16, 16);
 
-$$("#drawboard>div").forEach((cell) =>
-  cell.addEventListener("mouseover", function (e) {
-    this.style.backgroundColor = "cyan";
-  })
-);
+$("#resize-button").addEventListener("click", function (e) {
+  let sizeStr = prompt("Enter board size: {rows},{cols}");
+  let [nRows, nCols] = sizeStr.split(",");
+  console.log(`${nRows}, ${nCols}`);
+  fillBoard(nRows, nCols);
+});
